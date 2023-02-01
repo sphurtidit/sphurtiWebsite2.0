ec79ebc43e910f3e58215992fbbf5a7460850d10
Tanishq <tanishqjain1652003@gmail.com>
2023-02-01 20:56:58 +0530

    navbar added

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Contact.css'
+
+function Contact() {
+  return (
+    <div>
+      This is contact us page.
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/pages/LiveResult/LiveResult.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LiveResult.css'
+
+function LiveResult() {
+  return (
+    <div>
+      This is Live Result page.
+    </div>
+  )
+}
+
+export default LiveResult
