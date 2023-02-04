54ea0a16be98670e5af41621e48b71cb68c2179a
shreyal0007 <shreyaljain0007@gmail.com>
2023-02-04 13:00:12 +0530

    contacts page added

---
+++ src/components/Contactcards/Studentcontact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Contactcard.css";
+
+const Studentcontact= ({ state }) => {
+	return (
+
+		<div className="cardcontainer">
+			<img src={state.thumbnail} alt="not available"></img>
+			<div className="div2">
+				<p className="name">{state.name}</p>
+                <p className="number">{state.phone}</p>
+			</div>
+		</div>
+	);
+};
+
+export default Studentcontact;
