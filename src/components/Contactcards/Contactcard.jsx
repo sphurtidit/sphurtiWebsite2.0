6bc3de9c0e7168c97205330ba83fe8ba1a946df3
shreyal0007 <shreyaljain0007@gmail.com>
2023-02-04 17:00:28 +0530

    changes

--- src/components/Contactcards/Contactcard.jsx
+++ src/components/Contactcards/Contactcard.jsx
@@ -3,15 +3,14 @@ import "./Contactcard.css";
 
 const Contactcard = ({ data }) => {
 	return (
-
-		<div className="cardcontainer">
-			<img src={data.thumbnail} alt="not available"></img>
-			<div className="div2">
-				<p className="name">{data.name}</p>
-                <p className="number">{data.phone}</p>
-			</div>
-		</div>
-	);
+    <div className="cardcontainer">
+      <img src={data.imageUrl} alt="not available"></img>
+      <div className="div2">
+        <p className="name">{data.name}</p>
+        <p className="number">{data.phone}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Contactcard;
--- src/components/Contactcards/Studentcontact.jsx
+++ src/components/Contactcards/Studentcontact.jsx
@@ -3,15 +3,14 @@ import "./Contactcard.css";
 
 const Studentcontact= ({ state }) => {
 	return (
-
-		<div className="cardcontainer">
-			<img src={state.thumbnail} alt="not available"></img>
-			<div className="div2">
-				<p className="name">{state.name}</p>
-                <p className="number">{state.phone}</p>
-			</div>
-		</div>
-	);
+    <div className="cardcontainer">
+      <img src={data.imageUrl} alt="not available"></img>
+      <div className="div2">
+        <p className="name">{state.name}</p>
+        <p className="number">{state.phone}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Studentcontact;
