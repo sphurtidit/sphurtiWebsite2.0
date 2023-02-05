76f8975b3886a0195f9b9abc7202460b3e7df4a2
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-02-05 13:11:53 +0530

    removed duplicate entry

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,24 +1,23 @@
 import Contactwrapper from "../../components/Contactcards/Contactwrapper";
-import image1 from "../../assets/rectangle26.png"
+import image1 from "../../assets/rectangle26.png";
 
 function Contact() {
 	return (
-    <div
-    style={{
-        backgroundImage: `url(${image1})`,
-        backgroundRepeat: "no-repeat",
-        width: "100%",
-        position: "relative",
-        width: "100%",
-        padding: "2rem 1.5rem 3rem 1.5rem",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-    }}
-    >
-      <Contactwrapper />
-    </div>
-  );
+		<div
+			style={{
+				backgroundImage: `url(${image1})`,
+				backgroundRepeat: "no-repeat",
+				width: "100%",
+				position: "relative",
+				padding: "2rem 1.5rem 3rem 1.5rem",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Contactwrapper />
+		</div>
+	);
 }
 
 export default Contact;
