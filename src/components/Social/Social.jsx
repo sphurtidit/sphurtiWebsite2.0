c0e8ba2a87187a043d77a1817cd4e47c31072cd4
shreyal0007 <shreyaljain0007@gmail.com>
2023-02-06 21:43:09 +0530

    changes

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -3,16 +3,25 @@ import facebook from "../../assets/facebook.png";
 import instagram from "../../assets/instagram.png";
 import twitter from "../../assets/twitter.png";
 import youtube from "../../assets/youtube.png";
+import { Link } from "react-router-dom";
 
 const Social = () => {
 	return (
-		<div className="Social">
-			<img src={facebook} alt="socail" />
-			<img src={instagram} alt="socail" />
-			<img src={twitter} alt="socail" />
-			<img src={youtube} alt="socail" />
-		</div>
-	);
+    <div className="Social">
+      <Link to="">
+        <img src={facebook} alt="socail" />
+      </Link>
+      <Link to="https://www.instagram.com/dit_sphurti_/">
+        <img src={instagram} alt="socail" />
+      </Link>
+      <Link to="">
+        <img src={twitter} alt="socail" />
+      </Link>
+      <Link to="">
+        <img src={youtube} alt="socail" />
+      </Link>
+    </div>
+  );
 };
 
 export default Social;
--- src/pages/EventPage/EventPage.jsx
+++ src/pages/EventPage/EventPage.jsx
@@ -2,30 +2,35 @@ import { useLocation } from "react-router-dom";
 import "./EventPage.css";
 import Circles from "../../components/Circles/Circles";
 
+
 function EventPage() {
 	const location = useLocation();
 	return (
-		<div className="EventPage">
-			<Circles />
-			<h1>{location.state.data.name}</h1>
-			<div>
-				<ul>
-					<li>name: {location.state.data.name}</li>
-					<li>prize: {location.state.data.prize_money}</li>
-					<li>registration fees: {location.state.data.registration_fees}</li>
-					<li>registration link: {location.state.data.registration_link}</li>
-					<li>
-						rules:
-						<ul style={{ padding: "1rem" }}>
-							{location.state.data.rules.map((rule, id) => {
-								return <li key={id}>{rule}</li>;
-							})}
-						</ul>
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+    <div className="EventPage">
+      <Circles />
+      <h1>{location.state.data.name}</h1>
+      <div>
+        <ul>
+          <li>name: {location.state.data.name}</li>
+          <li>prize: {location.state.data.prize_money}</li>
+          <li>registration fees: {location.state.data.registration_fees}</li>
+          <li>registration link: {location.state.data.registration_link}</li>
+          <li>
+            rules:
+            <ul style={{ padding: "1rem" }}>
+              {location.state.data.rules.map((rule, id) => {
+                return <li key={id}>{rule}</li>;
+              })}
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+      <div className="buttondiv">
+        <button className="button">Resister now</button>
+      </div>
+    </div>
+  );
 }
 
 export default EventPage;
