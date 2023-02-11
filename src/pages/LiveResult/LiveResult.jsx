76a37a48b80dfc1c379b5f6335fbaaaf8b55d104
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-02-11 17:47:07 +0530

    added awaiting results when all results are empty

--- src/pages/LiveResult/LiveResult.jsx
+++ src/pages/LiveResult/LiveResult.jsx
@@ -28,62 +28,86 @@ const LiveResult = () => {
 	return (
 		<div className="LiveResultsPage" id="live">
 			<h1 className="liveresults">Live Results</h1>
-			{data && <div className="mainbadminton">
-				<p className="heading">Badminton</p>
-				<div className="badmintondiv">
-					{data &&
-						data.map((item, id) => {
-							return <img src={item} key={id} className="images"></img>;
-						})}
+			{data && (
+				<div className="mainbadminton">
+					<p className="heading">Badminton</p>
+					<div className="badmintondiv">
+						{data &&
+							data.map((item, id) => {
+								return <img src={item} key={id} className="images"></img>;
+							})}
+					</div>
 				</div>
-			</div>}
-			{data1  && data1.length > 0 && <div className="mainbasketball">
-				<p className="heading">Basketball</p>
-				<div className="basketballdiv">
-					{data1 &&
-						data1.map((item, id) => {
-							return <img src={item} key={id} className="images"></img>;
-						})}
+			)}
+			{data1 && data1.length > 0 && (
+				<div className="mainbasketball">
+					<p className="heading">Basketball</p>
+					<div className="basketballdiv">
+						{data1 &&
+							data1.map((item, id) => {
+								return <img src={item} key={id} className="images"></img>;
+							})}
+					</div>
 				</div>
-			</div>}
-			{data2 && data2.length > 0 && <div className="mainfootball">
-				<p className="heading">Football</p>
-				<div className="footballdiv">
-					{data2 &&
-						data2.map((item, id) => {
-							return <img src={item} key={id} className="images"></img>;
-						})}
+			)}
+			{data2 && data2.length > 0 && (
+				<div className="mainfootball">
+					<p className="heading">Football</p>
+					<div className="footballdiv">
+						{data2 &&
+							data2.map((item, id) => {
+								return <img src={item} key={id} className="images"></img>;
+							})}
+					</div>
 				</div>
-			</div>}
-			{data3 && data3.length > 0 && <div className="maincricket">
-				<p className="heading">Cricket</p>
-				<div className="cricketdiv">
-					{data3 &&
-						data3.map((item, id) => {
-							return <img src={item} key={id} className="images"></img>;
-						})}
+			)}
+			{data3 && data3.length > 0 && (
+				<div className="maincricket">
+					<p className="heading">Cricket</p>
+					<div className="cricketdiv">
+						{data3 &&
+							data3.map((item, id) => {
+								return <img src={item} key={id} className="images"></img>;
+							})}
+					</div>
 				</div>
-			</div>}
-			{data4 && data4.length > 0 && <div className="maintabletennis">
-				<p className="heading">Table Tennis</p>
-				<div className="tabletennisdiv">
-					{data4 &&
-						data4.map((item, id) => {
-							return <img src={item} key={id} className="images"></img>;
-						})}
+			)}
+			{data4 && data4.length > 0 && (
+				<div className="maintabletennis">
+					<p className="heading">Table Tennis</p>
+					<div className="tabletennisdiv">
+						{data4 &&
+							data4.map((item, id) => {
+								return <img src={item} key={id} className="images"></img>;
+							})}
+					</div>
 				</div>
-			</div>}
-			{data5 && data5.length > 0 && <div className="mainvollyball">
-				<p className="heading">Vollyball</p>
-				<div className="vollyballdiv">
-					{data5 &&
-						data5.map((item, id) => {
-							return <img src={item} key={id} className="images"></img>;
-						})}
+			)}
+			{data5 && data5.length > 0 && (
+				<div className="mainvollyball">
+					<p className="heading">Vollyball</p>
+					<div className="vollyballdiv">
+						{data5 &&
+							data5.map((item, id) => {
+								return <img src={item} key={id} className="images"></img>;
+							})}
+					</div>
 				</div>
-			</div>}
+			)}
+			{!data && !data1 && !data2 && !data3 && !data4 && !data5 && (
+				<p
+					style={{
+						color: "grey",
+						textAlign: "center",
+						fontSize: "2rem",
+						margin: "2rem 0",
+					}}
+				>
+					Awaiting results...
+				</p>
+			)}
 		</div>
 	);
-}
+};
 
 export default LiveResult;
